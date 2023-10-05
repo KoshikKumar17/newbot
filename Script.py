97a047b748ac35768ddcb3aaca060aca692dc1d1
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2023-10-05 08:28:58 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,6 @@
+class script(object):
+    START_TEXT = """Hey! I am a bot made by @TheKoshik. Click __/help__ to know more **about** the bot.
+
+**© @TheKoshik"""
+    HELP_TEXT = """**Still in Testing. Coming soon.**"""
+    ABOUT_TEXT = """**SOON..**"""
