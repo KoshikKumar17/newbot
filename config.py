5cfcff76d7d4aa64e36a19ce1f6a39afd6b9fd2d
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-03-05 08:36:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 # Bot information
 class Config(object):
-  APP_ID = int(os.environ.get("APP_ID", ""))
+  API_ID = int(os.environ.get("API_ID", ""))
   API_HASH = os.environ.get("API_HASH", "")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   OWNER_ID = 1162032262
